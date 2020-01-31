bd323284c9fb9fda11339e4623098291449f68af
Alex Leonov <spawnleon@gmail.com>
2020-01-31 16:54:13 +0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -10,12 +10,13 @@ module.exports = {
     require('css-mqpacker'),
     require('cssnano')({
       preset: [
-        'default', {
+        'default',
+        {
           discardComments: {
-            removeAll: true
-          }
-        }
-      ]
-    })
-  ]
+            removeAll: true,
+          },
+        },
+      ],
+    }),
+  ],
 };
